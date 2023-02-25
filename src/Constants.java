103447b925623748444cc42a1cf6b7509ec1ac90
notaroboticsenthusiast <91231142+notaroboticsenthusiast@users.noreply.github.com>
2023-02-25 09:57:41 -0600

    ist borked tha mtujvch

---
+++ src/Constants.java
@@ -0,0 +1,18 @@
+/*
+ * Fake Constants meant to simulate FRC stufferuni
+ */
+
+public class Constants {
+    public static class AStar {
+        /**
+         * Half the length of the field relative to the origin. Positive for Max, Negative for Min.
+         * Measured in cm.
+         */
+        public static final int FIELD_X = 1654 / 2;
+        /**
+         * Half the width of the field relative to the origin. Positive for Max, Negative for Min.
+         * Measured in cm.
+         */
+        public static final int FIELD_Y = 802 / 2;
+    }
+}
---
+++ src/main.java
@@ -0,0 +1,9 @@
+public class main {
+
+    public static void main(String[] args) {
+
+        AStar aStar = new AStar(0,0, 100, 200);
+        System.out.println(aStar.generateAStarPath());
+
+    }
+}
