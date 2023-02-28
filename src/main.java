6139453b2bc7b71265e599e8e81cbecf505ed89e
notaroboticsenthusiast <91231142+notaroboticsenthusiast@users.noreply.github.com>
2023-02-28 16:07:19 -0600

    help v2

--- src/main.java
+++ src/main.java
@@ -1,10 +1,11 @@
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.ArrayList;
 
 public class main {
 
     public static void main(String[] args) {
 
-        AStar boundaries = new AStar(Constants.AStar.FIELD_X*2-1, Constants.AStar.FIELD_Y*2-1, 0, 0);
+        AStar boundaries = new AStar(0, 0, 123, 45);
         AtomicReference<Path> path = boundaries.generateAStarPath();
         double maxMemUse = 0;
         while (!path.get().isPathGenerated()) {
@@ -13,6 +14,13 @@ public class main {
                 System.out.println("New max mem usage at " + System.currentTimeMillis() + ": " + Utils.getUsedMem() + "Kb");
             }
         }
+
+        for (int i=0; i<path.get().getPathList().size(); i++) {
+//            for (int j=0; j<path.get().getPathList().get(i).length; j++) {
+                System.out.println(path.get().getPathList().get(i)[0] + ", " + path.get().getPathList().get(i)[1]);
+//            }
+        }
+
         //AStar sizeTest = new AStar(0, 0, Constants.AStar.FIELD_X*2-1, Constants.AStar.FIELD_Y*2-1);
         //System.out.println(sizeTest.generateAStarPath());
 
